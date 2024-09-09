9a67eed37613b29bd999225f303949d042967aea
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-09-09 11:13:27 +0100

    add restx for api documentation

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from flask_restx import Api
 
 
 def create_app():
 
     app = Flask(__name__)
+
+
+    api=Api(app,
+        title='ExamBook API',
+        version='1.0',
+        description='A RESTAPI for the online Examination app',
+        authorizations='authorizations',
+        security='Bearer Auth'
+    )
 
     return app
