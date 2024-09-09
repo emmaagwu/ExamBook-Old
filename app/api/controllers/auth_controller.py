afa0669a7ec731643a565a200661fb6036f33ec4
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-09-09 14:55:37 +0100

    implement the signup logic

--- app/api/controllers/auth_controller.py
+++ app/api/controllers/auth_controller.py
@@ -1,14 +1,57 @@
 from flask_restx import Namespace,Resource,fields
+from ..models.users import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import request
+from http import HTTPStatus
+from werkzeug.exceptions import Conflict,BadRequest
+
 
 auth_namespace=Namespace('auth', description='a namespace for authentication')
+
+SignUp_model = auth_namespace.model(
+    'SignUp', {
+        'username': fields.String(required=True, description='Username of the user'),
+        'email': fields.String(required=True, description='Email of the user'),
+        'password': fields.String(required=True, description='Password of the user'),
+})
+
+User_model = auth_namespace.model(
+    'User', {
+        'id':fields.Integer(),
+        'username':fields.String(required=True, description='The username of the user'),
+        'email': fields.String(required=True, description='The email of the user'),
+        'password_hash': fields.String(required=True, description='The password of the user'),
+        'created_at': fields.DateTime(description='The created time of the user'),
+        'updated_at': fields.DateTime(description='The updated time of the user'),
+    }
+)
 
 
 
 @auth_namespace.route('/signup')
 class Signup(Resource):
     
+    @auth_namespace.expect(SignUp_model)
+    @auth_namespace.marshal_with(User_model)
     def post(self):
         """
             Create a new user
         """
-        pass
+        data = request.get_json()
+
+        try:
+
+            new_user=User(
+                username=data.get('username'),
+                email=data.get('email'),
+                password_hash=generate_password_hash(data.get('password'))
+            )
+
+            new_user.save()
+
+            return new_user, HTTPStatus.CREATED
+
+        except Exception:
+            raise Conflict(f"User with email {data.get('email')} already exists") from None
+        
+
--- app/api/models/users.py
+++ app/api/models/users.py
@@ -1,12 +1,21 @@
 from ..utils.db import db
-
+from datetime import datetime
 
 class User(db.Model):
     __tablename__='users'
     id=db.Column(db.Integer(), primary_key=True)
     username=db.Column(db.String(45), unique=True, nullable=False)
     email=db.Column(db.String(45), unique=True, nullable=False)
-    password_hash=db.Column(db.String(100), nullable=False)
+    password_hash=db.Column(db.String(255), nullable=False)
+    created_at=db.Column(db.DateTime(), default=datetime.utcnow)
+    updated_at=db.Column(db.DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def __repr__(self):
         return f'<User: {self.username}>'
+
+    def save(self):
+        """
+            Saves the user to the database.
+        """
+        db.session.add(self)
+        db.session.commit()
